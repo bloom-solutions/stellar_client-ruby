7571714c770a64de5dbc38d3711c8b5c893b5236
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-26 15:02:18 +0800

    Simplify bridge_client by using api_client_base

--- lib/bridge_client/client.rb
+++ lib/bridge_client/client.rb
@@ -1,11 +1,10 @@
 module BridgeClient
   class Client
 
-    include Virtus.model
-    attribute :host, String
-
     include APIClientBase::Client.module(default_opts: :default_opts)
     api_action :send_payment
+
+    attribute :host, String
 
     private
 
