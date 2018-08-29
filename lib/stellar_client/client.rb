4094109d3b068825789936b7f05e33fd5ef377df
Ace Subido and Ramon Tayag <dev+ace+ramon@bloom.solutions>
2018-08-29 18:28:13 +0800

    Add client#withdraw

--- lib/stellar_client/client.rb
+++ lib/stellar_client/client.rb
@@ -5,6 +5,13 @@ module StellarClient
     api_action :send_payment
     api_action :get_toml
 
+    def withdraw(opts = {})
+      transfer_host = get_toml.toml["TRANSFER_SERVER"]
+      request = WithdrawRequest.new(opts.merge(host: transfer_host))
+      raw_response = request.()
+      WithdrawResponse.new(raw_response: raw_response)
+    end
+
     attribute :bridge_host, String
 
     private
